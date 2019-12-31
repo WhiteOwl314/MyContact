718828b2ef248407de67fb41a53547335b4626a8
WhiteOwl314 <parksj914@naver.com>
2019-12-31 16:11:05 +0900

    Solve Error

--- src/main/java/com/fastcampus/javaallinone/project4/mycontact/MycontactApplication.java
+++ src/main/java/com/fastcampus/javaallinone/project4/mycontact/MycontactApplication.java
@@ -1,9 +1,12 @@
 package com.fastcampus.javaallinone.project4.mycontact;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MycontactApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/fastcampus/javaallinone/project4/mycontact/MycontactApplicationTests.java
+++ src/test/java/com/fastcampus/javaallinone/project4/mycontact/MycontactApplicationTests.java
@@ -1,9 +1,12 @@
 package com.fastcampus.javaallinone.project4.mycontact;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //dataSource 설정을 설정해주지 않아 발생한 오류 해결
 class MycontactApplicationTests {
 
     @Test
