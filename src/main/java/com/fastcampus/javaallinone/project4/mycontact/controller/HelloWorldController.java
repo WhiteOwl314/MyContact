d8126d8316ed90d67b6abe792b5684ca9cff23d9
WhiteOwl314 <parksj914@naver.com>
2019-12-31 14:53:01 +0900

    JPA 준비 의존성 주입

--- src/main/java/com/fastcampus/javaallinone/project4/mycontact/controller/HelloWorldController.java
+++ src/main/java/com/fastcampus/javaallinone/project4/mycontact/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.fastcampus.javaallinone.project4.mycontact.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @ResponseBody //API형식*/
 @RestController
 public class HelloWorldController {
+    //Get -> Post
     @GetMapping(value = "/api/helloWorld")
     public String HelloWorld(){
         return "Hello World";
