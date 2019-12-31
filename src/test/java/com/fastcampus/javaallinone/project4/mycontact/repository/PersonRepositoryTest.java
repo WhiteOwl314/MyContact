d26dc154cca8ad9de17803ca3324a943c40b5504
WhiteOwl314 <parksj914@naver.com>
2019-12-31 15:55:09 +0900

    Copy PersonRepositoryTest

--- src/test/java/com/fastcampus/javaallinone/project4/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project4/mycontact/repository/PersonRepositoryTest.java
@@ -1,7 +1,27 @@
 package com.fastcampus.javaallinone.project4.mycontact.repository;
 
+import com.fastcampus.javaallinone.project4.mycontact.domain.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class PersonRepositoryTest {
 
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void crud(){
+
+        Person person = new Person();
+
+        personRepository.save(person);
+
+        System.out.println(personRepository.findAll());
+    }
 }
