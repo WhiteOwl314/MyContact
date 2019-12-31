845fa1b94b07393d9c6af999cff5f8be16927c45
WhiteOwl314 <parksj914@naver.com>
2019-12-31 15:06:00 +0900

    PersonRepository 생성

---
+++ src/main/java/com/fastcampus/javaallinone/project4/mycontact/domain/Person.java
@@ -0,0 +1,16 @@
+package com.fastcampus.javaallinone.project4.mycontact.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity //값을 다루고 있는 객체
+public class Person {
+
+    @Id
+    @GeneratedValue //자동생성
+    private Long id;
+    private String name;
+    private int age;
+
+}
